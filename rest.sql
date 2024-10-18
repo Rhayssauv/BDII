60cca1588dd05715a9217a3b16de9342dad1f11c
Rhayssauv <161035203+Rhayssauv@users.noreply.github.com>
2024-10-18 15:26:28 -0300

    Add files via upload

--- rest.sql
+++ rest.sql
@@ -1,39 +1,56 @@
+drop schema restaurante;
 create schema restaurante;
 use restaurante;
 
 create table cliente (
-  id_cliente int not null,
-  nome varchar(100) not null,
+  idcliente int not null,
+  nome varchar(100),
   cpf varchar(14),
-  endereco varchar(150),
-  cod_mesa tinyint (2)
+  endereco varchar(150)
 );
-alter table cliente add constraint pk_cliente primary key (id_cliente);
-alter table cliente add constraint fk_mesa_cliente foreign key cod_mesa references mesa (cod_mesa);
+alter table cliente add constraint pk_cliente primary key (idcliente);
 
 create table atendente (
-  matricula int(3) zerofill not null,
-  nome varchar(100),
-  salario double not null,
+  matricula smallint(3) zerofill not null,
+  nome varchar(100) not null,
+  salario decimal(7,2) not null,
   telefone varchar(25) not null
 );
+alter table atendente add constraint pk_atendente primary key (matricula);
 
 create table mesa (
-  cod_mesa tinyint(2) zerofill not null
+  num_mesa tinyint(2) zerofill not null
 );
-alter table mesa add constraint pk_mesa primary key (cod_mesa);
-alter table mesa add constraint chk_cod check (cod_mesa >= 01 and cod_mesa <= 99);
+alter table mesa add constraint pk_mesa primary key (num_mesa);
+alter table mesa add constraint chk_cod check (num_mesa >= 01 and num_mesa <= 99);
 
 create table item (
   cod_item int not null,
   nome varchar(45) not null,
-  preco decimal(4,2) not null
+  preco decimal(3,2) not null
 );
 alter table item add constraint pk_item primary key (cod_item);
 
 create table pedido (
-  cod_pedido int auto_increment not null,
-  inicio datetime,
-  fim datetime,
-  duracao time_to_second(timediff(time(fim), (inicio)))
+  cod_pedido int auto_increment primary key not null,
+  idcliente int not null,
+  matricula smallint(3) zerofill not null,
+  num_mesa tinyint(2) zerofill not null,
+  inicio time,
+  fim time
+  
 );
+-- alter table pedido add constraint pk_pedido primary key (cod_pedido);
+alter table pedido add constraint fk_pedido_mesa foreign key (num_mesa) references mesa (num_mesa);
+alter table pedido add constraint fk_pedido_cliente foreign key (idcliente) references cliente (idcliente);
+alter table pedido add constraint fk_pedido_atendente foreign key (matricula) references atendente (matricula);
+
+
+create table pedido_item (
+pedido_cod_pedido int not null,
+item_cod_item int not null,
+quantidade tinyint
+);
+alter table pedido_item add constraint pk_pedido_item primary key (pedido_cod_pedido, item_cod_item);
+alter table pedido_item add constraint fk_pedido_item_item foreign key (item_cod_item) references item (cod_item);
+alter table pedido_item add constraint fk_pedido_item_pedido foreign key (pedido_cod_pedido) references pedido (cod_pedido);
